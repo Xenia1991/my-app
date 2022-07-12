2ab347ea148cc6374ea3dceef88105f3d969d4ad
Xenia <andreeva19-91@mail.ru>
2022-07-12 07:30:51 +0300

    add branch lesson-2

--- src/Components/users.jsx
+++ src/Components/users.jsx
@@ -7,12 +7,12 @@ const Users =() => {
     const [users, setUser] = useState(allUsers);
     let userNumber = users.length;
 
-    const getHead = () => {
+    const getLabel = () => {
        return (
-           userNumber===1 ? <h1> <span className="badge bg-primary">{userNumber} человек тусанет с тобой сегодня </span></h1> : 
-           userNumber===0 ? <h1> <span className="badge bg-danger">никто с тобой не тусанет </span></h1> : 
-           userNumber<=4 ? <h1> <span className="badge bg-primary">{userNumber} человека тусанет с тобой сегодня </span></h1> : 
-           <h1> <span className="badge bg-primary">{userNumber} человек тусанут с тобой сегодня </span></h1>
+           userNumber===1 ? <h1> <span className="badge bg-primary">{userNumber} человек тусанет с тобой сегодня </span></h1> 
+           : userNumber===0 ? <h1> <span className="badge bg-danger">никто с тобой не тусанет </span></h1> 
+           : userNumber<=4 ? <h1> <span className="badge bg-primary">{userNumber} человека тусанет с тобой сегодня </span></h1> 
+           : <h1> <span className="badge bg-primary">{userNumber} человек тусанут с тобой сегодня </span></h1>
        )
     }
     
@@ -27,33 +27,33 @@ const Users =() => {
     }
 
     return (
-    <>
-    {getHead()}
+        <>
+        {getLabel()}
 
-    <table className="table table-light" hidden = {users.length===0 ? "hidden" : ""}>
-        <thead>
-            <tr>
-                <th scope="col">Имя</th>
-                <th scope="col">Качества</th>
-                <th scope="col">Профессия</th>
-                <th scope="col">Встретился, раз</th>
-                <th scope="col">Оценка</th>
-                <th scope="col">  </th>
-            </tr>  
-        </thead>
+        <table className="table table-light" hidden = {users.length===0 ? "hidden" : ""}>
+            <thead>
+                <tr>
+                    <th scope="col">Имя</th>
+                    <th scope="col">Качества</th>
+                    <th scope="col">Профессия</th>
+                    <th scope="col">Встретился, раз</th>
+                    <th scope="col">Оценка</th>
+                    <th scope="col">  </th>
+                </tr>  
+            </thead>
         
-        <tbody>
-            {users.map((user)=> <tr className="table-light" key={user._id}>
-                <td> {user.name} </td>
-                <td> {user.qualities.map((quality) => {return <span className={getBadgeClasses(quality.name)}>{` ${quality.name} `}</span>})} </td>
-                <td> {user.profession.name} </td>
-                <td> {user.completedMeetings} </td>
-                <td> {`${user.rate} /5`} </td>
-                <td> <button className="btn btn-danger" onClick={() => handleUserChange(user._id)}> delete </button></td>
-            </tr>)}
-        </tbody>
-    </table>
-    </>
+            <tbody>
+                {users.map((user)=> <tr className="table-light" key={user._id}>
+                    <td> {user.name} </td>
+                    <td> {user.qualities.map((quality) => {return <span className={getBadgeClasses(quality.name)}>{` ${quality.name} `}</span>})} </td>
+                    <td> {user.profession.name} </td>
+                    <td> {user.completedMeetings} </td>
+                    <td> {`${user.rate} /5`} </td>
+                    <td> <button className="btn btn-danger" onClick={() => handleUserChange(user._id)}> delete </button></td>
+                </tr>)}
+            </tbody>
+        </table>
+        </>
     ) 
 };
 
