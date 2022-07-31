391cb23e73c03fd069c7ce3806f897b3eeeaa4f8
Xenia <andreeva19-91@mail.ru>
2022-07-31 17:55:50 +0300

    pagination && proptypes

--- src/App.jsx
+++ src/App.jsx
@@ -2,34 +2,39 @@ import React, { useState } from "react";
 import Users from "./Components/users";
 import API from "../src/api";
 import SearchStatus from "./Components/searchStatus";
-import Bookmark from "./Components/bookmark";
 
 const App = () => {
-  const allUsers = API.users.fetchAll();
-  const [users, setUsers] = useState(allUsers);
+    const allUsers = API.users.fetchAll();
+    const [users, setUsers] = useState(allUsers);
 
-  const handleUserChange = (id) => {
-    setUsers((prevState) => prevState.filter((user) => user._id !== id));
-  };
+    const handleUserChange = (id) => {
+        setUsers((prevState) => prevState.filter((user) => user._id !== id));
+    };
 
-  const handleBookmark = (id) => {
-    console.log(id)
-    setUsers((prevState) =>
-      prevState.map((user) => {
-        if (user._id === id) {
-          return { ...user, bookmark: !user.bookmark };
-        }
-        return user;
-      })
+    const handleBookmark = (id) => {
+        console.log(id);
+        setUsers((prevState) =>
+            prevState.map((user) => {
+                if (user._id === id) {
+                    return { ...user, bookmark: !user.bookmark };
+                }
+                return user;
+            })
+        );
+    };
+
+    return (
+        <>
+            <SearchStatus length={users.length} />
+            {users.length > 0 && (
+                <Users
+                    onDelete={handleUserChange}
+                    onBookmark={handleBookmark}
+                    users={users}
+                />
+            )}
+        </>
     );
-  };
-
-  return (
-    <>
-      <SearchStatus length={users.length} />
-      {users.length > 0 && <Users onDelete={handleUserChange} onBookmark={handleBookmark} users={users} />}
-    </>
-  );
 };
 
 export default App;
--- src/Components/qualities.jsx
+++ src/Components/qualities.jsx
@@ -1,13 +1,16 @@
 import React from "react";
 import "bootstrap/dist/css/bootstrap.css";
 
-const Qualities = ({qualities}) => {
+const Qualities = ({ qualities }) => {
     const userQuality = qualities.map((quality) => {
         return (
-            <span className={`badge bg-${quality.color} m-1`} key={quality._id}> {quality.name}</span>
+            <span className={`badge bg-${quality.color} m-1`} key={quality._id}>
+                {" "}
+                {quality.name}
+            </span>
         );
-    })
+    });
     return userQuality;
-}
+};
 
 export default Qualities;
